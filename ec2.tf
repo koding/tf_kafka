64ba2ed3a63b951341e9746c674399ff6529e6e8
Cihangir SAVAS <cihangir@koding.com>
2016-06-23 02:58:06 +0300

    ec2: add sample ec2 creation template

---
+++ ec2.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "kafka" {
+  count         = "${length(split(",", var.private_ips))}"
+  ami           = "${var.ami_id}"
+  instance_type = "${var.aws_instance_type}"
+  key_name      = "${aws_key_pair.cihangir.key_name}"
+
+  ebs_optimized = "${var.ebs_optimized}"
+  subnet_id = "${var.aws_subnet_subnet_id}"
+
+  # (Optional) A list of security group IDs to associate with.
+  vpc_security_group_ids = ["${split(",", var.vpc_security_group_ids)}"]
+
+  user_data = "${element(template_file.kafka_cloud_init_file.*.rendered, count.index)}"
+
+  private_ip = "${element(split(",", var.private_ips), count.index)}"
+
+  tags {
+    Name = "${format("zookeeper-%03d", count.index + 1)}"
+  }
+}
