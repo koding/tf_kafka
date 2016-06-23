1429aeae37e62a297f5d11105845ff4dd3c42d31
Cihangir SAVAS <cihangir@koding.com>
2016-06-24 02:48:37 +0300

    template: encode with base64

--- templates.tf
+++ templates.tf
@@ -2,9 +2,9 @@ resource "template_file" "kafka_cloud_init_file" {
   template = "${file("data/kafka.yaml.tpl")}"
 
   vars = {
-    kafka_install_script    = "${template_file.kafka_install.rendered}"
-    kafka_init_script       = "${file("data/kafka.init")}"
-    region                  = "${var.region}"
+    kafka_install_script_b64    = "${base64encode(template_file.kafka_install.rendered)}"
+    kafka_init_script_b64       = "${base64encode(file("data/kafka.init"))}"
+    region                      = "${var.region}"
   }
 }
 
