f86815badba70bafbf2a86573086d63eb04fd693
Cihangir SAVAS <cihangir@koding.com>
2016-06-29 20:42:58 +0300

    templates: add kafka server properties and zookeeper conn string for kafka

--- templates.tf
+++ templates.tf
@@ -5,6 +5,7 @@ resource "template_file" "kafka_cloud_init_file" {
     kafka_install_script_b64      = "${base64encode(template_file.kafka_install.rendered)}"
     kafka_init_script_b64         = "${base64encode(file("data/kafka.init"))}"
     kafka_broker_id_generator_b64 = "${base64encode(file("data/brokerid.sh"))}"
+    kafka_server_properties_b64   = "${base64encode(file("data/server.properties"))}"
     region                        = "${var.region}"
   }
 }
@@ -13,7 +14,8 @@ resource "template_file" "kafka_install" {
   template = "${file("data/kafka_install.sh.tpl")}"
 
   vars = {
-    SCALA_VERSION = "${var.scala_version}"
-    KAFKA_VERSION = "${var.kafka_version}"
+    SCALA_VERSION      = "${var.scala_version}"
+    KAFKA_VERSION      = "${var.kafka_version}"
+    ZOOKEEPER_CONN_STR = "${join(",",formatlist("%s:%s", split(",", var.zookeeper_ips), var.zookeeper_port))}"
   }
 }
