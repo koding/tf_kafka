46d94b5ff00d9673ba80cdc8ff04fb88642af2ca
Cihangir SAVAS <cihangir@koding.com>
2016-07-26 00:34:15 +0300

    vars: update vars

--- vars.tf
+++ vars.tf
@@ -1,46 +1,81 @@
-variable "name" {
-  default = "kafka_cluster"
+variable "env_name" {
+  default = "koding-zookeeper"
 }
 
-variable "ebs_optimized" {
-  default = false
+variable "app_name" {
+  default = "koding"
 }
 
-variable "aws_subnet_subnet_id" {
-  default = "subnet-8b195cd3"
-}
-
-variable "aws_instance_type" {
-  default = "c3.xlarge"
-}
-
-variable "ami_id" {
-  description = "aws-elasticbeanstalk-amzn-2016.03.0.x86_64-php56-hvm-201603311549"
-  default     = "ami-b04842da"
-}
-
-variable "private_ips" {
-  default = "10.0.0.121,10.0.0.122,10.0.0.123"
-variable "zookeeper_ips" {
-  default = "10.0.0.121"
-}
-
-variable "zookeeper_port" {
-  default = "2181"
+variable "solution_stack_name" {
+  default = "64bit Amazon Linux 2016.03 v2.1.2 running PHP 5.6"
 }
 
 variable "key_pair_name" {
-  default = "cihangir"
+  default = "key-koding-eb-deployment-dev"
 }
 
-variable "vpc_security_group_ids" {
-    default = "sg-9846f2e3"
+variable "iaminstanceprofile" {
+  default = "aws-elasticbeanstalk-ec2-role"
 }
 
-variable "scala_version" {
-    default = "2.11"
+variable "s3_bucket_name" {
+  default = "koding-dev-test-exhibitor"
 }
 
-variable "kafka_version" {
-    default = "0.10.0.0"
+variable "ami_id" {
+  default = "ami-032ba014"
+}
+
+variable "instance_type" {
+  default = "c3.2xlarge"
+}
+
+variable "vpc_id" {
+  default = "vpc-54a53133"
+}
+
+variable "vpc_subnets" {
+  default = "subnet-2a2e1b00,subnet-f4dbefac"
+}
+
+variable "elb_subnet_ids" {
+  default = "subnet-2a2e1b00,subnet-f4dbefac"
+}
+
+variable "healthCheckEndpoint" {
+  default = "HTTP:8181/exhibitor/v1"
+}
+
+variable "deployment_type" {
+  description = "Choose a deployment policy for application version deployments. (AllAtOnce, Rolling, RollingWithAdditionalBatch, Immutable)"
+  default     = "Rolling"
+}
+
+variable "deployment_command_timeout" {
+  description = "Number of seconds to wait for an instance to complete executing commands. (1 to 3600)"
+  default     = "600"
+}
+
+variable "deployment_batch_size_type" {
+  description = "The type of number that is specified in BatchSize. (Percentage, Fixed)"
+  default     = "Fixed"
+}
+
+variable "deployment_batch_size" {
+  description = " Percentage or fixed number of Amazon EC2 instances in the Auto Scaling group on which to simultaneously perform deployments. Valid values vary per BatchSizeType setting. (1 to 100 (Percentage). 1 to aws:autoscaling:asg::MaxSize (Fixed))"
+  default     = "1"
+}
+
+variable "service_role_name" {
+  description = "The name of an IAM role that Elastic Beanstalk uses to manage resources for the environment."
+  default     = "aws-elasticbeanstalk-service-role"
+}
+
+# Instance Settings
+variable "ec2_key_name" {
+  default = "ec2_key"
+}
+
+variable "aws_security_group_prefix" {
+  default = "sec-group-"
 }
