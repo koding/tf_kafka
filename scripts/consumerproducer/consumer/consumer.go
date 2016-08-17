4567b54d427a7ec2b10457b46e2a23b070f84d07
mehmetalisavas <mehmetali.savas@yandex.com>
2016-08-17 18:21:55 +0300

    script: move into a function to use as goroutine

--- scripts/consumerproducer/consumer/consumer.go
+++ scripts/consumerproducer/consumer/consumer.go
@@ -4,8 +4,14 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"sync"
+	"time"
 
 	"github.com/Shopify/sarama"
+)
+
+var (
+	msgCount int
 )
 
 func main() {
@@ -39,25 +45,54 @@ func main() {
 	signal.Notify(signals, os.Interrupt)
 
 	// Count how many message processed
-	msgCount := 0
+	// msgCount := 0
 
 	// Get signnal for finish
 	doneCh := make(chan struct{})
-	go func() {
-		for {
-			select {
-			case err := <-consumer.Errors():
-				fmt.Println(err)
-			case msg := <-consumer.Messages():
-				msgCount++
-				fmt.Println("Received messages", string(msg.Key), string(msg.Value))
-			case <-signals:
-				fmt.Println("Interrupt is detected")
-				doneCh <- struct{}{}
-			}
-		}
-	}()
 
-	<-doneCh
+	var wg sync.WaitGroup
+	wg.Add(3)
+
+	// warn to return all goroutines when Interrupt signal triggered
+	go handleCtrlC(signals, doneCh)
+
+	for index := 0; index < 3; index++ {
+		go consume(consumer, doneCh, &wg)
+	}
+
+	// <-doneCh
+	wg.Wait()
+
 	fmt.Println("Processed", msgCount, "messages")
+}
+
+func consume(consumer sarama.PartitionConsumer, doneCh chan struct{}, wg *sync.WaitGroup) {
+	defer wg.Done()
+	msgCount = 0
+
+	for {
+
+		select {
+		case err := <-consumer.Errors():
+			fmt.Println(err)
+		case msg := <-consumer.Messages():
+			msgCount++
+			fmt.Println("Received messages", string(msg.Key), string(msg.Value))
+			// case <-signals:
+			// 	fmt.Println("Interrupt is detected")
+			// 	doneCh <- struct{}{}
+		case <-doneCh:
+			fmt.Println("DONE CHANNEL TRIGGERED")
+			return
+		case <-time.After(10 * time.Second):
+			fmt.Println("TIME AFTER WORKS")
+			return
+		}
+	}
+}
+
+func handleCtrlC(c chan os.Signal, cc chan struct{}) {
+	// handle ctrl+c event here
+	<-c
+	close(cc)
 }
